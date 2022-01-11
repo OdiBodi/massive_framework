0c003a39744e6cbd2936d99f601afb35108da6da
Andrey Gursky <>
2022-01-11 21:38:20 +0300

    added editor scripts

---
+++ Assets/MassiveFramework/Scripts/Editor/PostBuild.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using Facebook.Unity.Editor.iOS.Xcode;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace Squares
+{
+    public class PostBuild
+    {
+#if UNITY_IOS
+        [PostProcessBuild(1000)]
+        public static void ModifyPBXProject(BuildTarget target, string path)
+        {
+            var project = new PBXProject();
+            var projectPath = PBXProject.GetPBXProjectPath(path);
+            project.ReadFromFile(projectPath);
+
+            var targetName = PBXProject.GetUnityTargetName();
+            var targetGuid = project.TargetGuidByName(targetName);
+
+            project.SetBuildProperty(targetGuid, "SWIFT_VERSION", "5.1");
+
+            File.WriteAllText(projectPath, project.WriteToString());
+        }
+#endif
+    }
+}
