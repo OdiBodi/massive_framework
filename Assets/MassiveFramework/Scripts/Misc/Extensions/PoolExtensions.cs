6b3939553130c80df2a3a0be21b8d4fd8ca83475
Andrey Gursky <>
2022-02-13 21:51:05 +0300

    updated pool extensions

--- Assets/MassiveFramework/Scripts/Misc/Extensions/PoolExtensions.cs
+++ Assets/MassiveFramework/Scripts/Misc/Extensions/PoolExtensions.cs
@@ -6,14 +6,28 @@ namespace MassiveCore.Framework
 {
     public static class VfxExtensions
     {
+        public static T Get<T>(this Pool pool, string id, Vector3 position, Quaternion rotation, Vector3 scale,
+            Action<T> onPrepare = null) where T : BaseMonoBehaviour
+        {
+            var obj = pool.Get<T>(id);
+            obj.CacheTransform.position = position;
+            obj.CacheTransform.rotation = rotation;
+            obj.CacheTransform.localScale = scale;
+            onPrepare?.Invoke(obj);
+            return obj;
+        }
+
+        public static T Get<T>(this Pool pool, string id, Camera camera, Vector3 position, Quaternion rotation,
+            Vector3 scale, Action<T> onPrepare = null) where T : BaseMonoBehaviour
+        {
+            var worldPoint = camera.ViewportToWorldPoint(position);
+            return pool.Get(id, worldPoint, rotation, scale, onPrepare);
+        }
+
         public static Task PlayVfx(this Pool pool, string id, Vector3 position, Quaternion rotation, Vector3 scale,
             Action<Vfx> onPrepare = null)
         {
-            var vfx = pool.Get<Vfx>(id);
-            vfx.CacheTransform.position = position;
-            vfx.CacheTransform.rotation = rotation;
-            vfx.CacheTransform.localScale = scale;
-            onPrepare?.Invoke(vfx);
+            var vfx = pool.Get(id, position, rotation, scale, onPrepare);
             return vfx.Play();
         }
 
