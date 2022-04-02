51f0c3ff0b96dccd909512cb37d3a17ae1e9be8d
Andrey Gursky <>
2022-04-02 22:24:17 +0300

    added vibration clickable button

---
+++ Assets/MassiveFramework/Scripts/Ui/Controls/Buttons/VibrationClickableButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace MassiveCore.Framework
+{
+    public class VibrationClickableButton : BaseMonoBehaviour
+    {
+        [Inject]
+        private readonly IVibrations vibrations;
+
+        [SerializeField]
+        private BaseButton button;
+
+        [SerializeField]
+        private string vibrationId;
+
+        private void Start()
+        {
+            SubscribeOnButton();
+        }
+
+        private void SubscribeOnButton()
+        {
+            button.OnClicked += () => vibrations.Vibrate(vibrationId);
+        }
+    }
+}
