98d0e97234a2112efba1bcd29180b23fa0acc454
Andrey Gursky <>
2022-04-10 21:58:36 +0300

    updated nice vibrations plugin

--- Assets/MassiveFramework/Plugins/NiceVibrations/Demo/DemoAssets/CarDemo/Scripts/CarDemoManager.cs
+++ Assets/MassiveFramework/Plugins/NiceVibrations/Demo/DemoAssets/CarDemo/Scripts/CarDemoManager.cs
@@ -71,7 +71,6 @@ namespace Lofelt.NiceVibrations
                     _carStartedAt = Time.time;
                     _lastStartClickAt = Time.time;
 
-                    HapticController.fallbackPreset = HapticPatterns.PresetType.HeavyImpact;
                     HapticPatterns.PlayConstant(_knobValue, _knobValue, MaximumPowerDuration);
                     CarEngineAudioSource.Play();
                 }
