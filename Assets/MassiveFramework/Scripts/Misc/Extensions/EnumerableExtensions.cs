f7b8af85a3e078e33e0e2c169c732a8ba8f974b6
Andrey Gursky <>
2022-04-13 00:35:57 +0300

    added ToString to enumerable extensions

--- Assets/MassiveFramework/Scripts/Misc/Extensions/EnumerableExtensions.cs
+++ Assets/MassiveFramework/Scripts/Misc/Extensions/EnumerableExtensions.cs
@@ -1,12 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using ModestTree;
 using Random = UnityEngine.Random;
 
 namespace MassiveCore.Framework
 {
     public static class EnumerableExtensions
     {
+        public static string ToString<T>(this IEnumerable<T> array, Func<T, string> argument)
+        {
+            var builder = new StringBuilder();
+            if (array.Any())
+            {
+                var first = array.First();
+                builder.Append(argument(first));
+                foreach (var element in array.Except(first))
+                {
+                    builder.Append($",{argument(element)}");
+                }
+            }
+            return $"[{builder}]";
+        }
+
         public static void ForEach<T>(this IEnumerable<T> array, Action<T> predicate)
         {
             foreach (var element in array)
