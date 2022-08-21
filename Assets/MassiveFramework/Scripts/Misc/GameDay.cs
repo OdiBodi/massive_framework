84e73e60356369a69e7d24c968005171a1cf9450
Andrey Gursky <>
2022-08-21 22:42:15 +0300

    added game day class removed profile extensions

---
+++ Assets/MassiveFramework/Scripts/Misc/GameDay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MassiveCore.Framework
+{
+    public class GameDay
+    {
+        private readonly IProfile profile;
+
+        public GameDay(IProfile profile)
+        {
+            this.profile = profile;
+        }
+
+        public int Day()
+        {
+            return (int) (DateTime.Now - profile.FirstLaunchDate).TotalDays;
+        }
+    }
+}
