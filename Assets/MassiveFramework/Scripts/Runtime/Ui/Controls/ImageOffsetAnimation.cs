aa07f1f8a9e9b875b29b7e105740d90a6476485f
Andrey Gursky <gyrichus1@gmail.com>
2022-10-21 23:33:51 +0300

    added image offset animation

---
+++ Assets/MassiveFramework/Scripts/Runtime/Ui/Controls/ImageOffsetAnimation.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MassiveCore.Framework
+{
+    [RequireComponent(typeof(Image))]
+    public class ImageOffsetAnimation : BaseMonoBehaviour
+    {
+        [SerializeField]
+        private Vector2 direction = Vector2.one;
+
+        [SerializeField]
+        private float speed = 5f;
+
+        private void Awake()
+        {
+            var material = CacheImage.material;
+            var duration = 1f / speed;
+            CacheImage.material = new Material(material);
+            CacheImage.material.DOOffset(direction, duration).SetEase(Ease.Linear).SetLoops(-1);
+        }
+    }
+}
