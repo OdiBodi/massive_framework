5f6ff0aed169d67dfe0b994e76c220ac061f54fd
Andrey Gursky <gyrichus1@gmail.com>
2023-01-02 19:13:37 +0300

    added short text number

---
+++ Assets/MassiveFramework/Scripts/Runtime/Misc/ShortTextNumber.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace MassiveCore.Framework
+{
+    public class ShortTextNumber
+    {
+        private readonly int _number;
+
+        public ShortTextNumber(int number)
+        {
+            _number = number;
+        }
+
+        public string Text()
+        {
+            var result = string.Empty;
+            if (_number >= 1000000)
+            {
+                var number = _number / 1000000f;
+                result = number.ToString("#.#m", NumberFormatInfo.InvariantInfo);
+            }
+            else if (_number >= 1000)
+            {
+                var number = _number / 1000f;
+                result = number.ToString("#.#k", NumberFormatInfo.InvariantInfo);
+            }
+            else
+            {
+                result = _number.ToString();
+            }
+            return result;
+        }
+    }
+}
