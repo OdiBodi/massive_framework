2f948c126c330f609948b9857138e5f6dd909594
Andrey Gursky <gyrichus1@gmail.com>
2023-01-02 19:13:11 +0300

    added custom json converters

---
+++ Assets/MassiveFramework/Scripts/Runtime/Converters/CustomJsonConverter.cs
@@ -0,0 +1,50 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace MassiveCore.Framework
+{
+    public class CustomJsonConverter<T> : JsonConverter
+    {
+        private class ContractResolver : DefaultContractResolver
+        {
+            protected override JsonContract CreateContract(Type objectType)
+            {
+                if (typeof(T).IsAssignableFrom(objectType))
+                {
+                    var contract = CreateObjectContract(objectType);
+                    contract.Converter = null; // Prevent infinite recursion
+                    return contract;
+                }
+                return base.CreateContract(objectType);
+            }
+        }
+
+        private JsonSerializer DefaultSerializer
+        {
+            get
+            {
+                var resolver = new ContractResolver();
+                var settings = new JsonSerializerSettings { ContractResolver = resolver };
+                var serializer = JsonSerializer.CreateDefault(settings);
+                return serializer;
+            }
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(T).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var result = DefaultSerializer.Deserialize(reader, objectType);
+            return result;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            DefaultSerializer.Serialize(writer, value);
+        }
+    }
+}
---
+++ Assets/MassiveFramework/Scripts/Runtime/Converters/CustomJsonTypeConverter.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace MassiveCore.Framework
+{
+    public class CustomJsonTypeConverter<T> : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                var json = value.ToString();
+                var result = JsonConvert.DeserializeObject<T>(json);
+                return result;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
