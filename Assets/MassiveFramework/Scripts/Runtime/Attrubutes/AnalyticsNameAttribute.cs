83a6bba350b1155e796cb3c1fe1052653d80c97b
Andrey Gursky <gyrichus1@gmail.com>
2023-06-17 19:25:44 +0300

    refactored attributes

--- Assets/MassiveFramework/Scripts/Runtime/Attrubutes/AnalyticsNameAttribute.cs
+++ Assets/MassiveFramework/Scripts/Runtime/Attrubutes/AnalyticsNameAttribute.cs
@@ -5,10 +5,11 @@ namespace MassiveCore.Framework.Runtime
     [AttributeUsage(AttributeTargets.Field)]
     public class AnalyticsNameAttribute : Attribute
     {
-        public string Name { get; }
         public AnalyticsNameAttribute(string name)
         {
             Name = name;
         }
+
+        public string Name { get; }
     }
 }
--- Assets/MassiveFramework/Scripts/Runtime/Attrubutes/NumberAttribute.cs
+++ Assets/MassiveFramework/Scripts/Runtime/Attrubutes/NumberAttribute.cs
@@ -5,10 +5,11 @@ namespace MassiveCore.Framework.Runtime
     [AttributeUsage(AttributeTargets.Field)]
     public class NumberAttribute : Attribute
     {
-        public int Number { get; }
         public NumberAttribute(int number)
         {
             Number = number;
         }
+
+        public int Number { get; }
     }
 }
