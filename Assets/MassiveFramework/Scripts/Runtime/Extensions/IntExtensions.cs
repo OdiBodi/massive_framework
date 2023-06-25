5155b1616085256bd2baa6dfa29ed65069c63917
Andrey Gursky <gyrichus1@gmail.com>
2023-06-26 02:57:07 +0300

    refactored int extensions

--- Assets/MassiveFramework/Scripts/Runtime/Extensions/IntExtensions.cs
+++ Assets/MassiveFramework/Scripts/Runtime/Extensions/IntExtensions.cs
@@ -6,7 +6,7 @@ namespace MassiveCore.Framework.Runtime
     {
         public static int RandomRangeExcept(this int except, int min, int max)
         {
-            if (max-min <= 1)
+            if (max - min <= 1)
             {
                 return min;
             }
