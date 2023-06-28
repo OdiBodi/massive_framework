da35a308f020c87a80cc6ce6da360229ee01b1d0
Andrey Gursky <gyrichus1@gmail.com>
2023-06-28 19:53:47 +0300

    updated dictionary extensions

--- Assets/MassiveFramework/Scripts/Runtime/Extensions/DictionaryExtensions.cs
+++ Assets/MassiveFramework/Scripts/Runtime/Extensions/DictionaryExtensions.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MassiveCore.Framework.Runtime
 {
     public static class DictionaryExtensions
     {
-        public static string ToFormattedString<K, V>(this IDictionary<K, V> dictionary, string format = "{0}:{1}\n")
+        public static string ToFormattedString<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, string format = "{0}:{1}\n")
         {
             var sb = new StringBuilder();
             foreach (var pair in dictionary)
@@ -14,6 +16,19 @@ namespace MassiveCore.Framework.Runtime
                 sb.Append(formattedText);
             }
             return sb.ToString();
+        }
+
+        public static Dictionary<TKey, int> Sum<TKey>(this IDictionary<TKey, int> dictionary, IDictionary<TKey, int> other)
+        {
+            var result = dictionary.Concat(other).GroupBy(kv => kv.Key)
+                .Select(g => new KeyValuePair<TKey, int>(g.Key, g.Sum(kv => kv.Value)))
+                .ToDictionary(kv => kv.Key, kv => kv.Value);
+            return result;
+        }
+
+        public static Dictionary<TKey, TValue> Combine<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, IDictionary<TKey, TValue> other)
+        {
+            return dictionary.Concat(other).ToDictionary(kv => kv.Key, kv => kv.Value);
         }
     }
 }
