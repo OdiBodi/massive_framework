9b463e55c9cfbc6d1e11d61ded532a0ba2f8c13f
Andrey Gursky <gyrichus1@gmail.com>
2023-06-28 19:57:47 +0300

    added initializer to input service

---
+++ Assets/MassiveFramework/Scripts/Runtime/Services/Input/InputInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace MassiveCore.Framework.Runtime
+{
+    public class InputInitializer : ServiceInitializer
+    {
+        [SerializeField]
+        private bool _multitouch;
+
+        public override UniTask<bool> Initialize()
+        {
+            Input.multiTouchEnabled = _multitouch;
+            CompleteInitialize(true);
+            return base.Initialize();
+        }
+    }
+}
