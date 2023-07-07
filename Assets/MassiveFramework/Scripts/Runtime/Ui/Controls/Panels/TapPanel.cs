ba1112e6adb17c30dcdb3ada94feb70079d03e94
Andrey Gursky <gyrichus1@gmail.com>
2023-07-07 19:34:12 +0300

    updated ui tap panel

--- Assets/MassiveFramework/Scripts/Runtime/Ui/Controls/Panels/TapPanel.cs
+++ Assets/MassiveFramework/Scripts/Runtime/Ui/Controls/Panels/TapPanel.cs
@@ -1,13 +1,21 @@
 using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace MassiveCore.Framework.Runtime
 {
     public class TapPanel : BaseMonoBehaviour, IPointerDownHandler
     {
+        [SerializeField]
+        private bool _active = true;
+
         public event Action Tapped;
 
-        public bool Active { get; set; } = true;
+        public bool Active
+        {
+            get => _active;
+            set => _active = value;
+        }
 
         public void OnPointerDown(PointerEventData eventData)
         {
