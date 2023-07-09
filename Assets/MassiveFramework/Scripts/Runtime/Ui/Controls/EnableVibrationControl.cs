7ac246ffde807428660b76058f0d5fc0d962bf0a
Andrey Gursky <gyrichus1@gmail.com>
2023-07-09 19:12:53 +0300

    added enable vibration control

---
+++ Assets/MassiveFramework/Scripts/Runtime/Ui/Controls/EnableVibrationControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace MassiveCore.Framework.Runtime
+{
+    public class EnableVibrationControl : BaseMonoBehaviour
+    {
+        [Inject]
+        private readonly IVibrations _vibrations;
+
+        [SerializeField]
+        private string _vibrationId = "vibration";
+
+        private void OnEnable()
+        {
+            _vibrations.Vibrate(_vibrationId);
+        }
+    }
+}
