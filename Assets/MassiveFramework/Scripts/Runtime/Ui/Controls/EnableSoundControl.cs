0ee0437cd6c539a68aecadee59800c8124ac5d4b
Andrey Gursky <gyrichus1@gmail.com>
2023-07-09 19:12:33 +0300

    added enable sound control

---
+++ Assets/MassiveFramework/Scripts/Runtime/Ui/Controls/EnableSoundControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace MassiveCore.Framework.Runtime
+{
+    public class EnableSoundControl : BaseMonoBehaviour
+    {
+        [Inject]
+        private readonly ISounds _sounds;
+
+        [SerializeField]
+        private string _soundId = "sound";
+
+        private void OnEnable()
+        {
+            _sounds.PlaySound(_soundId);
+        }
+    }
+}
